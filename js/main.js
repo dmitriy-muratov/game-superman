e248bdca2dff087deb08838e033b7a1dab31ab8b
dmitriy_muratov <muratovdima@gmail.com>
2014-06-26 16:53:03 +0300

    add bonus cakes, create counter, update hero animation, and other fixes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,90 @@
 jQuery(document).ready(function(){
 
-
-    
-   /* function getRandom(num){
-        var my_num = Math.floor((Math.random()*shell_count));
-        return my_num;
+    // rand
+    function randomFunction (min, max){
+        min = parseInt(min);
+        max = parseInt(max);
+        return Math.floor( Math.random() * (max - min + 1)) + min;
     }
-    var numRand = getRandom(shell_count);*/
-    
-    var i = 0;
-    setInterval( function() {
-        i++
-        var bg_speed = 0+i*3;
-        $('#wrapper').css('background-position', -bg_speed+'px' + ' 90%');  
-        
-        /*var hero_position_down = Math.floor($("#hero" ).offset().top);
-        $('#hero').css('top', hero_position_down+1);*/
-        
-    }, 30);
-    
-    
-    $('#wrapper').bind('click', function(){
 
-        var hero_position = Math.floor($("#hero" ).offset().top);
-        $('#hero').animate({
-            'top': hero_position-150
-        }, 300); 
-        var hero_position = Math.floor($("#hero" ).offset().top);
-        $('#hero').animate({
-            'top': hero_position
-        }, 600); 
-        
+    var i = 0;
+    var bg_speed;
+    setInterval( function() {
+        if (bg_speed >= 1350) {
+            i = 0;
+        }
+        i++
+        bg_speed = 0+i*6;
+        $('#wrapper').css('background-position', -bg_speed+'px' + ' 90%'); 
+        //var hero_position_down = Math.floor($("#hero" ).offset().top);
+        //$('#hero').css('top', hero_position_down+1);
+    }, 60);
+    
+    var moveItemOnClick = true;
+    $('#wrapper').bind('click', function(){
+        if(moveItemOnClick) {
+            moveItemOnClick = false
+            var hero_position = Math.floor($("#hero" ).offset().top);
+            $('#hero').animate({
+                'top': hero_position-150
+            }, 300); 
+            $('#hero').animate({
+                'top': hero_position
+            }, 600, function(){
+                moveItemOnClick = true;
+            });
+        }
     });
     
+    function removeMushroom(){
+        $(this).remove();
+    }
     
-    var enemy;
-    var enemy_position;
-    var enemy_position_left;
-    //setInterval( function() {
-        enemy = $('#wrapper').append('<span class="mushroom" />');  
-        enemy_position = enemy.position();
-        enemy_position_left = enemy_position.left;
-    //}, 1500);
+    var pointsCounter = $('#points_counter');
+    $(pointsCounter).text('100');
     
-    setInterval( function() { 
-        enemy = $('.mushroom');
-        enemy_position = enemy.position();
-        enemy_position_left = enemy_position.left;
-        $(enemy).css('left', enemy_position_left-10);       
-    }, 30);
+    // check game items coordinates and count points
+    function gameItems(elClass, pointsFunction){
+        $(elClass).each(function(){
+            var jumpPoints = parseInt($(pointsCounter).html());
+            // hero coordinates
+            var topHero = $('#hero').position().top;
+            var bottomHero = topHero + $('#hero').height();
+            var leftHero = $('#hero').position().left;
+            var rightHero = $('#hero').position().left + $('#hero').width();
+            // mushroom coordinates
+            var topCoins = $(elClass).position().top;
+            var leftCoins = $(elClass).position().left;
+            // if coordinates equal
+            if(topCoins > topHero && topCoins < bottomHero && leftCoins > leftHero && leftCoins < rightHero){
+                $(this).remove();
+                $(pointsCounter).text( pointsFunction( jumpPoints ) );
+            }
+        });
+    }
+    
+    //create game items
+    var windowWidth = $('body').width();
+    var randNum = randomFunction (2000, 8000);
+    function gameItemMove(elClass) {
+        var runItem = randomFunction (2000, 5000);
+        var gameItem = $('<span class="' + elClass + '" />');
+        $('#wrapper .inner').append(gameItem);    
+        jQuery(gameItem).animate({right: windowWidth}, runItem, 'linear', removeMushroom);
+        if(gameItem.offset.right == windowWidth) {
+            $(this).remove();
+        }
+        setInterval(function() { 
+            gameItems('.coins', function(j) { return j += 1; });
+            gameItems('.mushroom', function(j) { return j -= 2; });
+        }, 30); 
+    }    
+    setInterval( function() {
+        gameItemMove('mushroom');
+    }, randomFunction (2000, 6000));    
+    setInterval( function() {
+        gameItemMove('coins');
+    }, randomFunction (5000, 15000));
     
     //restart game
     $('#restart-btn').click(function() {
